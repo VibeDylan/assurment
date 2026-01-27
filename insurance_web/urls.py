a2c8bf175fde02d47349393fa682973a112a1e7c
VibeDylan <dylanbourbotte62@icloud.com>
2026-01-27 09:37:43 +0100

    add signup form and route

--- insurance_web/urls.py
+++ insurance_web/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='authentification/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='authentification/logout.html'), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- insurance_web/views.py
+++ insurance_web/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.isvalid():
+            user = form.save()
+            login(request, user)
+            return redirect('insurance_web:home')
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'authentification/signup.html', {'form': form})
