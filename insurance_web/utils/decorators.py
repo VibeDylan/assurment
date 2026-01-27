fb5a182ddb114098235ba33d8fc14e8e8806a753
VibeDylan <dylanbourbotte62@icloud.com>
2026-01-27 11:23:24 +0100

    add mixins based class

--- insurance_web/utils/decorators.py
+++ insurance_web/utils/decorators.py
@@ -16,3 +16,18 @@ def advice_required(view_func):
             return redirect('insurance_web:home')
         return view_func(request, *args, **kwargs)
     return _wrapped_view
+
+
+def advice_or_admin_required(view_func):
+    """Décorateur pour les vues réservées aux conseillers et admins."""
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('insurance_web:login')
+        
+        if not (request.user.profile.is_advice() or request.user.is_staff):
+            messages.error(request, "Vous n'avez pas les permissions nécessaires.")
+            return redirect('insurance_web:home')
+        
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
