ca7aa647beeebef353c7f49336544914f62ac301
VibeDylan <dylanbourbotte62@icloud.com>
2026-01-27 14:57:47 +0100

    add docutils for documentation

--- assurement/settings.py
+++ assurement/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.admindocs",
     'insurance_web',
 ]
 
@@ -30,6 +31,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',  # Required for admin docs
 ]
 
 ROOT_URLCONF = 'assurement.urls'
--- assurement/urls.py
+++ assurement/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),  # Must be before admin.site.urls
     path('admin/', admin.site.urls),
     path('', include('insurance_web.urls')),
 ]
